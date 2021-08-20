128cd5366ecaa7f2895bf0c8190b0afd9ed81f77
changsooooooooooo <lcs940621@gmail.com>
2021-08-21 02:03:06 +0900

    kakao pb3 cannot solved

--- kakao-intern/pb3/main.py
+++ kakao-intern/pb3/main.py
@@ -1,3 +1,7 @@
 from solution import solution
 
-print(solution("one4seveneight"))
+n, k = 8, 2
+cmd = ["D 2","C","U 3","C","D 4","C","U 2","Z","Z","U 1","C"]
+
+print(solution(n, k, cmd))
+
--- kakao-intern/pb3/solution.py
+++ kakao-intern/pb3/solution.py
@@ -1,18 +1,55 @@
-def solution(s):
-    answer_s = ''
-    char_part = ''
+from collections import deque
 
-    candidate = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
-    for i, c in enumerate(s):
-        if c.isnumeric():
-            answer_s += c
+def solution(n, k, cmd):
+    answer = ''
+    exist_list = [True for _ in range(n + 1)]
+    current_table = [i for i in range(n+1)]
+    cache = deque()
+    r_cus = k+1
+
+    for c in cmd:
+        if c[0] == "D":
+            r_cus += int(c[2])
             continue
-        char_part += c
-        for j in range(10):
-            if candidate[j] == char_part:
-                answer_s += str(j)
-                char_part = ""
-                break
+        if c[0] == "U":
+            r_cus -= int(c[2])
+            continue
+        if c == "C":
+            exist_list[current_table[r_cus]] = False
+            cache.append(current_table[r_cus])
+            if r_cus == len(current_table)-1:
+                current_table = current_table[:r_cus]
+                r_cus -= 1
+            else:
+                current_table = current_table[:r_cus]+current_table[r_cus+1:]
+            continue
 
-    return int(answer_s)
+        recent = cache.pop()
+        exist_list[recent] = True
+        if current_table[r_cus] < recent:
+            idx = len(current_table)
+            for i in range(r_cus, len(current_table)):
+                if recent < current_table[i]:
+                    idx = i
+                    break
+            current_table = current_table[:idx]+[recent]+current_table[idx:]
+            continue
+        if recent < current_table[r_cus]:
+            idx = 0
+            for i in range(r_cus, -1, -1):
+                if current_table[i] < recent:
+                    idx = i
+                    break
+            current_table = current_table[:idx+1]+[recent]+current_table[idx+1:]
+            r_cus += 1
+            continue
+
+    # print(cache)
+    exist_list = exist_list[1:]
+    for b in exist_list:
+        if b:
+            answer += "O"
+            continue
+        answer += "X"
+    return answer
